b5a0b1e6575115627359430882ed39cb62c842b8
Гладков Егор (Gladkov_ES) <Gladkov_ES@directum.ru>
2022-10-18 13:41:04 +0400

    Add movie manager

---
+++ MovieManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HalloweenDirectumBot;
+
+public struct Movie
+{
+  public string Description { get; }
+
+  public Stream MoviePosterPathStream { get; }
+
+  public Movie(string description, Stream moviePosterPathStream)
+  {
+    Description = description;
+    MoviePosterPathStream = moviePosterPathStream;
+  }
+}
+
+public class MovieManager
+{
+  private LinkedList<string> movieFolders;
+
+  public LinkedListNode<string> currentMovieFolder;
+
+  private static string GetMovieDescription(string movieFolder)
+  {
+    var movieDescriptionPath = Path.Combine(movieFolder, "description");
+    return File.ReadAllText(movieDescriptionPath);
+  }
+
+  private static Stream? GetMoviePoster(string movieFolder)
+  {
+    var moviePosterPath = Path.Combine(movieFolder, "poster.png");
+    return File.OpenRead(moviePosterPath);
+  }
+
+  public Movie GetNextMovie()
+  {
+    if (currentMovieFolder == null)
+      currentMovieFolder = movieFolders.First;
+
+    var movie = GetMovie();
+    currentMovieFolder = currentMovieFolder.Next;
+    return movie;
+  }
+
+  public Movie GetPreviousMovie()
+  {
+    if (currentMovieFolder.Value == null)
+      currentMovieFolder = movieFolders.Last;
+
+    var movie = GetMovie();
+    currentMovieFolder = currentMovieFolder.Previous;
+    return movie;
+  }
+
+  private Movie GetMovie()
+  {
+    var movieDescription = GetMovieDescription(currentMovieFolder.Value);
+    var moviePosterStream = GetMoviePoster(currentMovieFolder.Value) ?? Stream.Null;
+
+    return new Movie(movieDescription, moviePosterStream);
+  }
+
+  public MovieManager()
+  {
+    const string MoviesDirectory = "movies";
+    movieFolders = new LinkedList<string>(Directory.GetDirectories(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, MoviesDirectory)));
+  }
+}
--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ namespace HalloweenDirectumBot
 {
   public static class Program
   {
+    private static MovieManager _movieManager = new ();
+
     private static string[] prizeWords = new[]
     {
       "...сел медведь в машину и сгорел",
@@ -69,6 +71,36 @@ namespace HalloweenDirectumBot
 
       private static async Task HandleUpdate(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
       {
+        switch (update.Type)
+        {
+          case UpdateType.Message:
+          {
+            await ProcessMessageUpdate(bot, update, cancellationToken);
+            break;
+          }
+          case UpdateType.CallbackQuery:
+          case UpdateType.Unknown:
+          case UpdateType.InlineQuery:
+          case UpdateType.ChosenInlineResult:
+          case UpdateType.EditedMessage:
+          case UpdateType.ChannelPost:
+          case UpdateType.EditedChannelPost:
+          case UpdateType.ShippingQuery:
+          case UpdateType.PreCheckoutQuery:
+          case UpdateType.Poll:
+          case UpdateType.PollAnswer:
+          case UpdateType.MyChatMember:
+          case UpdateType.ChatMember:
+          case UpdateType.ChatJoinRequest:
+          default:
+            Console.WriteLine("IGNORE UPDATE");
+            return;
+        }
+      }
+
+      private static async Task ProcessMessageUpdate(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
+      {
+
         var message = update.Message;
         if (message is not { Type: MessageType.Text } || message.From is null)
           return;
@@ -78,75 +110,23 @@ namespace HalloweenDirectumBot
         {
           case BotCommands.Start:
           {
-            StickersManager.SendStickerAsync(bot, chatId, Emojis.Hello);
-            await bot.SendTextMessageAsync(
-              chatId,
-              $"Привет, {message.From.FirstName}!" +
-              $"\nВот ты и нашёл все баги. Чтобы узнать получишь ли ты приз нажми \"{BotCommands.Prize}\"." +
-              $"\nНо также ты можешь подобрать себе фильм для просмотра! Для этого нажми \"{BotCommands.Movie}\"",
-              replyMarkup: new ReplyKeyboardMarkup(new[]
-              {
-                new KeyboardButton(BotCommands.Prize),
-                new KeyboardButton(BotCommands.Movie)
-              }) { ResizeKeyboard = true },
-              cancellationToken: cancellationToken);
+            await SendWelcomeMessage(bot, cancellationToken, chatId, message);
             break;
           }
           case BotCommands.Prize:
           {
-            if (winners.Count < NumberOfWinners)
-            {
-              if (winners.TryGetValue(message.From.Id, out _))
-              {
-                StickersManager.SendStickerAsync(bot, chatId, Emojis.ExpressionlessFace);
-                await bot.SendTextMessageAsync(chatId,
-                  $"Слышь, {message.From.FirstName}, у тебя уже есть подарок, иди отсюда!!!",
-                  cancellationToken: cancellationToken);
-              }
-              else
-              {
-                var lastWinner = winners.LastOrDefault();
-                var number = lastWinner.Key == default ? 0 : lastWinner.Value + 1;
-                var word = prizeWords[number];
-
-                winners[message.From.Id] = number;
-                StickersManager.SendStickerAsync(bot, chatId, Emojis.GrinningFaceWithBigEyes);
-                await bot.SendTextMessageAsync(
-                  chatId,
-                  $"Поздравляю, {message.From.FirstName}! Ты один из счастливчиков, выигравших печенье от Эльвиры! Твой номер {number + 1} и твоя страшная фраза:\n \"{word}\"",
-                  cancellationToken: cancellationToken);
-              }
-            }
-            else
-            {
-              await bot.SendTextMessageAsync(
-                chatId,
-                "К сожалению, все призы уже разыграны. Приходи в следующем году!",
-                cancellationToken: cancellationToken);
-              StickersManager.SendStickerAsync(bot, chatId, Emojis.BlowingKiss);
-            }
+            await GivePrizeIfNeeded(bot, cancellationToken, message, chatId);
             break;
           }
           case BotCommands.Movie:
           {
-            const string MoviesDirectory = "movies";
-            const string PosterFilename = "poster.png";
-            const string MovieDescriptionFilename = "description";
-
-            var allMovies = Directory.GetDirectories(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, MoviesDirectory));
-            var randomMovieDirectory = allMovies[new Random(DateTime.Now.Millisecond).Next(0, allMovies.Length)];
-            var posterPath = Path.Combine(randomMovieDirectory, PosterFilename);
-            var description = File.ReadAllText(Path.Combine(randomMovieDirectory, MovieDescriptionFilename));
-            await using var stream = File.OpenRead(posterPath);
-            await bot.SendPhotoAsync(
-              chatId,
-              new InputOnlineFile(stream), description,
-              ParseMode.Markdown, cancellationToken: cancellationToken);
+            await ChooseMovie(bot, cancellationToken, chatId);
             break;
           }
           default:
           {
-            StickersManager.SendStickerAsync(bot, chatId, Emojis.PensiveFace);;
+            StickersManager.SendStickerAsync(bot, chatId, Emojis.PensiveFace);
+            ;
             await bot.SendTextMessageAsync(
               chatId,
               "Я не понимаю тебя, попробуй еще раз",
@@ -154,6 +134,72 @@ namespace HalloweenDirectumBot
             break;
           }
         }
+      }
+
+      private static async Task ChooseMovie(ITelegramBotClient bot, CancellationToken cancellationToken, long chatId)
+      {
+        var movie = _movieManager.GetNextMovie();
+        await using (movie.MoviePosterPathStream)
+        {
+          await bot.SendPhotoAsync(
+            chatId: chatId,
+            photo: new InputOnlineFile(movie.MoviePosterPathStream),
+            caption: movie.Description,
+            parseMode: ParseMode.Markdown,
+            cancellationToken: cancellationToken);
+        }
+      }
+
+      private static async Task GivePrizeIfNeeded(ITelegramBotClient bot, CancellationToken cancellationToken, Message message, long chatId)
+      {
+        if (winners.Count < NumberOfWinners)
+        {
+            if (winners.TryGetValue(message.From.Id, out _))
+            {
+              StickersManager.SendStickerAsync(bot, chatId, Emojis.ExpressionlessFace);
+              await bot.SendTextMessageAsync(chatId,
+                $"Слышь, {message.From.FirstName}, у тебя уже есть подарок, иди отсюда!!!",
+                cancellationToken: cancellationToken);
+            }
+            else
+            {
+              var lastWinner = winners.LastOrDefault();
+              var number = lastWinner.Key == default ? 0 : lastWinner.Value + 1;
+              var word = prizeWords[number];
+
+              winners[message.From.Id] = number;
+              StickersManager.SendStickerAsync(bot, chatId, Emojis.GrinningFaceWithBigEyes);
+              await bot.SendTextMessageAsync(
+                chatId,
+                $"Поздравляю, {message.From.FirstName}! Ты один из счастливчиков, выигравших печенье от Эльвиры! Твой номер {number + 1} и твоя страшная фраза:\n \"{word}\"",
+                cancellationToken: cancellationToken);
+            }
+        }
+        else
+        {
+          await bot.SendTextMessageAsync(
+            chatId,
+            "К сожалению, все призы уже разыграны. Приходи в следующем году!",
+            cancellationToken: cancellationToken);
+          StickersManager.SendStickerAsync(bot, chatId, Emojis.BlowingKiss);
+        }
+      }
+
+      private static async Task SendWelcomeMessage(ITelegramBotClient bot, CancellationToken cancellationToken, long chatId, Message message)
+      {
+
+        StickersManager.SendStickerAsync(bot, chatId, Emojis.Hello);
+        await bot.SendTextMessageAsync(
+          chatId,
+          $"Привет, {message.From.FirstName}!" +
+          $"\nВот ты и нашёл все баги. Чтобы узнать получишь ли ты приз нажми \"{BotCommands.Prize}\"." +
+          $"\nНо также ты можешь подобрать себе фильм для просмотра! Для этого нажми \"{BotCommands.Movie}\"",
+          replyMarkup: new ReplyKeyboardMarkup(new[]
+          {
+            new KeyboardButton(BotCommands.Prize),
+            new KeyboardButton(BotCommands.Movie)
+          }) { ResizeKeyboard = true },
+          cancellationToken: cancellationToken);
       }
 
       private static async Task HandleError(ITelegramBotClient bot, Exception exception, CancellationToken cancellationToken)
