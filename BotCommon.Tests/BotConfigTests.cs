7d8d209f7c9381a53d7b2b94d6d407df2efb4683
Егор <gladkovyegor@gmail.com>
2023-01-15 01:45:47 +0400

    Delete unused settings

--- BotCommon.Tests/BotConfigTests.cs
+++ BotCommon.Tests/BotConfigTests.cs
@@ -16,14 +16,7 @@ public class Tests
   [Test]
   public void Test1()
   {
-    const string Config = @"
-bot_token: 'test1'
-bot_name: 'Test bot'
-bot_about: 'Some phrase in profile description'
-bot_description: 'Some phrase before /start'
-bot_description_picture_full_filepath: 'test21'
-bot_picture_full_filepath: 'test3'
-bot_commands: 'test4'";
+    const string Config = @"bot_token: 'test1'";
     var config = new DeserializerBuilder()
         .WithNamingConvention(UnderscoredNamingConvention.Instance)
         .WithDuplicateKeyChecking()
@@ -39,24 +32,8 @@ bot_commands: 'test4'";
 
     Assert.Multiple(() =>
     {
-        Assert.That(config2.BotCommands, Is.EqualTo(config.BotCommands));
-        Assert.That(config2.BotAbout, Is.EqualTo(config.BotAbout));
-        Assert.That(config2.BotName, Is.EqualTo(config.BotName));
-        Assert.That(config2.BotDescription, Is.EqualTo(config.BotDescription));
-        Assert.That(config2.BotToken, Is.EqualTo(config.BotToken));
-        Assert.That(config2.BotPictureFullFilepath, Is.EqualTo(config.BotPictureFullFilepath));
-        Assert.That(config2.BotDescriptionPictureFullFilepath, Is.EqualTo(config.BotDescriptionPictureFullFilepath));
-    });
-
-    Assert.Multiple(() =>
-    {
-      Assert.That(string.IsNullOrEmpty(config2.BotCommands), Is.False);
-      Assert.That(string.IsNullOrEmpty(config2.BotAbout), Is.False);
-      Assert.That(string.IsNullOrEmpty(config2.BotName), Is.False);
-      Assert.That(string.IsNullOrEmpty(config2.BotDescription), Is.False);
       Assert.That(string.IsNullOrEmpty(config2.BotToken), Is.False);
-      Assert.That(string.IsNullOrEmpty(config2.BotPictureFullFilepath), Is.False);
-      Assert.That(string.IsNullOrEmpty(config2.BotDescriptionPictureFullFilepath), Is.False);
+      Assert.That(config2.BotToken, Is.EqualTo(config.BotToken));
     });
   }
 }
