580156d4ee40ff9f9b51b807b4144de7b70777fe
Dezodemius <gladkovyegor@gmail.com>
2023-02-07 15:55:03 +0400

    Add Program

--- Directum238Bot/Program.cs
+++ Directum238Bot/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Directum238Bot
+{
+  public static class Program
+  {
+    public static void Main(string[] args)
+    {
+      
+    }
+  }
+}
 
-Console.WriteLine("Hello, World!");
