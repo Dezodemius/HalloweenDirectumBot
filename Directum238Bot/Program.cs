46d96bd4031928dcb7cd27a8bd5898c18e4687e7
Dezodemius <gladkovyegor@gmail.com>
2023-03-03 20:20:42 +0400

    Remove unused code

--- Directum238Bot/Program.cs
+++ Directum238Bot/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using BotCommon;
 using BotCommon.KeepAlive;
@@ -9,9 +8,6 @@ using NLog;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.InputFiles;
-using Telegram.Bot.Types.ReplyMarkups;
-using File = System.IO.File;
 
 namespace Directum238Bot
 {
@@ -36,87 +32,7 @@ namespace Directum238Bot
       Environment.Exit(0);
     }
 
-    private static void StartAnimationScheduleMessage(ITelegramBotClient bot, string fileName, DateTime scheduleDate,
-      string message, string gifName)
-    {
-      bool needToNotificate;
-      var scheduledMessageInfoFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
-      if (!File.Exists(scheduledMessageInfoFilePath))
-      {
-        needToNotificate = true;
-      }
-      else
-      {
-        var scheduledMessageInfo = JsonConvert.DeserializeObject<ScheduledMessageInfo>(File.ReadAllText(scheduledMessageInfoFilePath));
-        needToNotificate = scheduledMessageInfo.NeedToNotificate;
-        scheduleDate = scheduledMessageInfo.MessageDateTime;
-      }
-
-      if (!needToNotificate)
-        return;
-
-      var timer = new System.Timers.Timer(TimeSpan.FromSeconds(5));
-      timer.AutoReset = true;
-      timer.Elapsed += (sender, b) =>
-      {
-        if (DateTime.Now.CompareTo(scheduleDate) < 0)
-          return;
-        System.Timers.Timer timer = null;
-        if (sender is System.Timers.Timer)
-          timer = (System.Timers.Timer)sender;
-        timer.Enabled = false;
-        var scheduleMessageInfo = new ScheduledMessageInfo { NeedToNotificate = false, };
-        File.WriteAllText(scheduledMessageInfoFilePath, JsonConvert.SerializeObject(scheduleMessageInfo));
-        var usersId = _activeUsersManager.GetAll();
-        var markup = new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData(Directum238BotResources.GoStartMenu, BotChatCommand.MainMenu));
-        foreach (var user in usersId)
-        {
-          var userInfo = bot.GetChatMemberAsync(user.BotUserId, user.BotUserId).Result;
-          log.Info($"Message sent to: {userInfo.User.FirstName} {userInfo.User}");
-          _ = bot.SendAnimationAsync(user.BotUserId,
-            animation: new InputOnlineFile(File.OpenRead(GetGifPath(gifName)), gifName)).Result;
-          _ = bot.SendTextMessageAsync(user.BotUserId,
-            text: message,
-            replyMarkup: markup,
-            parseMode: ParseMode.Markdown).Result;
-        }
-        timer.Dispose();
-      };
-      timer.Start();
-    }
-
-    private static void StartTextScheduleMessage(ITelegramBotClient bot, DateTime scheduleDate, string message)
-    {
-      var timer = new System.Timers.Timer(TimeSpan.FromSeconds(5));
-      timer.AutoReset = true;
-      timer.Elapsed += (a, b) =>
-      {
-        if (DateTime.Now.CompareTo(scheduleDate) < 0)
-          return;
-
-        var usersId = _activeUsersManager.GetAll();
-        var markup =
-            new InlineKeyboardMarkup(
-              InlineKeyboardButton.WithCallbackData(Directum238BotResources.GoStartMenu, BotChatCommand.MainMenu));
-        foreach (var user in usersId)
-        {
-          bot.SendTextMessageAsync(user.BotUserId,
-            text: message,
-            replyMarkup: markup,
-            parseMode: ParseMode.Markdown);
-        }
-        if (a is System.Timers.Timer timer)
-          timer.Dispose();
-      };
-      timer.Start();
-    }
-
-    private static string GetGifPath(string gifFileName)
-    {
-      return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "GIFs", gifFileName);
-    }
-
-    private static void PrepareForStartBot(TelegramBotClient bot)
+    private static void PrepareForStartBot(ITelegramBotClient bot)
     {
       _configManager = new BotConfigManager();
       _activeUsersManager = new ActiveUsersManager(_configManager.Config.DbConnectionString);
@@ -136,31 +52,19 @@ namespace Directum238Bot
         },
         ThrowPendingUpdates = true
       };
-      bot.OnApiResponseReceived += (client, args, token) =>
+      bot.OnApiResponseReceived += (_, args, _) =>
       {
-        LogManager.GetCurrentClassLogger().Debug(JsonConvert.SerializeObject(args));
+        log.Debug($" <<<< {JsonConvert.SerializeObject(args)}");
         return ValueTask.CompletedTask;
       };
-      bot.OnApiResponseReceived += (client, args, token) =>
+      bot.OnMakingApiRequest += (_, args, _) =>
       {
-        LogManager.GetCurrentClassLogger().Debug(JsonConvert.SerializeObject(args));
+        log.Debug($" >>>> {JsonConvert.SerializeObject(args)}");
         return ValueTask.CompletedTask;
       };
-      bot.OnMakingApiRequest += (client, args, token) =>
-      {
-        LogManager.GetCurrentClassLogger().Debug(JsonConvert.SerializeObject(args));
-        return ValueTask.CompletedTask;
-      };
-      // bot.StartReceiving(UpdateHandler, PollingErrorHandler, receiverOptions: opts);
+
       bot.StartReceiving<BotUpdateHandler>(receiverOptions: opts);
     }
-  }
-
-  public class ScheduledMessageInfo
-  {
-    public DateTime MessageDateTime { get; set; }
-    public bool NeedToNotificate { get; set; }
-    public string MessageName { get; set; }
   }
 }
 
