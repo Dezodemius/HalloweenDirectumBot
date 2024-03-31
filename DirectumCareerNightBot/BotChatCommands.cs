b059b7cca7999b242d02c6684ccfdd4c2fef7cfc
Dezodemius <gladkovyegor@gmail.com>
2024-03-31 23:56:04 +0400

    add bot commands

--- DirectumCareerNightBot/BotChatCommands.cs
+++ DirectumCareerNightBot/BotChatCommands.cs
@@ -3,5 +3,14 @@
 public class BotChatCommands
 {
     public const string Start = "/start";
-    public const string MainMenu = "/menu";
+    public const string MainMenu = "/m";
+    public const string Practice = "/p";
+    public const string Alumnus = "/a";
+    public const string Worker = "/w";
+    public const string Student = "/s";
+    public const string ITDeptWorker = "/i";
+    public const string Q15 = "/f";
+    public const string Raffle = "/r";
+    public const string Career = "/c";
+    public const string Socials = "/so";
 }
