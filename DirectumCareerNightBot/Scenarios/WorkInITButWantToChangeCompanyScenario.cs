1f6dda4b2e79e86fa05d681f0cba8a7d7f2c1f50
Dezodemius <gladkovyegor@gmail.com>
2024-04-01 01:38:06 +0400

    WorkInITButWantToChangeCompanyScenario stub

---
+++ DirectumCareerNightBot/Scenarios/WorkInITButWantToChangeCompanyScenario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BotCommon.Scenarios;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace DirectumCareerNightBot.Scenarios;
+
+public class WorkInITButWantToChangeCompanyScenario : AutoStepBotCommandScenario
+{
+    public override Guid Id { get; } = new ("EDC574B7-60DA-494B-A6F5-EC43E2BEE923");
+    public override string ScenarioCommand => string.Empty;
+    private Task StepAction1(ITelegramBotClient bot, Update update, long chatid)
+    {
+        // TODO: Давай знакомиться, напиши свои фамилию, имя и отчество
+    }
+    private Task StepAction2(ITelegramBotClient bot, Update update, long chatid)
+    {
+        // TODO: Как удобнее будет поддерживать связь с нашим HR-ом — напиши свои телефон/почту/vk
+    }
+    private Task StepAction3(ITelegramBotClient bot, Update update, long chatid)
+    {
+        // TODO: Расскажи в какой компании ты сейчас работаешь, на какой должности, если хочешь напиши подробнее про свой опыт
+    }
+    public WorkInITButWantToChangeCompanyScenario()
+    {
+        this.steps = new List<BotCommandScenarioStep>()
+        {
+            new (StepAction1),
+            new (StepAction2),
+            new (StepAction3),
+
+        }.GetEnumerator();
+    }
+}
