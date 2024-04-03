f68a2cafba87d7820202328019c9bd943dd44267
Dezodemius <gladkovyegor@gmail.com>
2024-04-04 01:35:56 +0400

    create quiz classes

---
+++ DirectumCareerNightBot/QuizQuestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DirectumCareerNightBot;
+
+public class QuizQuestion
+{
+    public Guid QuestionId { get; set; }
+    public string QuestionText { get; set; }
+    public List<QuizChoice> Choices { get; set; }
+    public Guid CorrectChoiceId { get; set; }
+}
+
+public class QuizChoice
+{
+    public Guid ChoiceId { get; set; }
+    public string ChoiceText { get; set; }
+}
