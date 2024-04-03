f449b3749c7a028772e88ecae603454dad35340c
Dezodemius <gladkovyegor@gmail.com>
2024-04-03 20:13:20 +0400

    add NotStudentButWantInITScenario scenario

--- DirectumCareerNightBot/Scenarios/NotStudentButWantInITScenario.cs
+++ DirectumCareerNightBot/Scenarios/NotStudentButWantInITScenario.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using BotCommon.Scenarios;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace DirectumCareerNightBot.Scenarios;
 
@@ -12,22 +14,36 @@ public class NotStudentButWantInITScenario : AutoStepBotCommandScenario
     public override Guid Id { get; } = new ("70E00609-A85A-4DB2-A518-5F3BFDA9992F");
     public override string ScenarioCommand => string.Empty;
 
-    private Task StepAction1(ITelegramBotClient bot, Update update, long chatid)
+    private async Task StepAction1(ITelegramBotClient bot, Update update, long chatId)
     {
-        // TODO: Давай знакомиться, напиши свои фамилию, имя и отчество
+        await bot.SendTextMessageAsync(chatId, BotMessages.IntroduceYourself, ParseMode.Markdown);
     }
-    private Task StepAction2(ITelegramBotClient bot, Update update, long chatid)
+    private async Task StepAction2(ITelegramBotClient bot, Update update, long chatId)
     {
-        // TODO: Как удобнее будет поддерживать связь с нашим HR-ом — напиши свои телефон/почту
+        Console.WriteLine(update.Message.Text);
+        await bot.SendTextMessageAsync(chatId, BotMessages.HowToContact, ParseMode.Markdown);
     }
-    private Task StepAction3(ITelegramBotClient bot, Update update, long chatid)
+    private async Task StepAction3(ITelegramBotClient bot, Update update, long chatId)
     {
-        // TODO: Расскажи о своём последнем месте работы, опыте
+        Console.WriteLine(update.Message.Text);
+        await bot.SendTextMessageAsync(chatId, BotMessages.TellAboutLastWork, ParseMode.Markdown);
     }
 
-    private Task StepAction4(ITelegramBotClient bot, Update update, long chatid)
+    private async Task StepAction4(ITelegramBotClient bot, Update update, long chatId)
     {
-        // TODO: что уже изучал в ИТ
+        Console.WriteLine(update.Message.Text);
+        await bot.SendTextMessageAsync(chatId, BotMessages.WhatYouAlreadyLearned, ParseMode.Markdown);
+    }
+
+    private async Task StepAction5(ITelegramBotClient bot, Update update, long chatId)
+    {
+        Console.WriteLine(update.Message.Text);
+        var buttons = new List<InlineKeyboardButton[]>
+        {
+            new[] { InlineKeyboardButton.WithCallbackData(BotMessages.MainMenuButton, BotChatCommands.MainMenu) }
+        };
+        var markup = new InlineKeyboardMarkup(buttons);
+        await bot.SendTextMessageAsync(chatId, BotMessages.ThankYouNotStudent, ParseMode.Markdown, replyMarkup: markup);
     }
 
     public NotStudentButWantInITScenario()
@@ -38,6 +54,7 @@ public class NotStudentButWantInITScenario : AutoStepBotCommandScenario
             new (StepAction2),
             new (StepAction3),
             new (StepAction4),
+            new (StepAction5),
 
         }.GetEnumerator();
     }
