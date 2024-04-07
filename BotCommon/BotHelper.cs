8766cf427bbe7083ebbbdc77a385631f208eda5e
Dezodemius <gladkovyegor@gmail.com>
2024-04-07 20:55:05 +0400

    create bot helper class

---
+++ BotCommon/BotHelper.cs
@@ -0,0 +1,27 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace BotCommon;
+
+public static class BotHelper
+{
+    public static User GetUserInfo(Update update)
+    {
+        return update.Type switch
+        {
+            UpdateType.Message => update.Message.From,
+            UpdateType.CallbackQuery => update.CallbackQuery.From,
+            _ => default
+        };
+    }
+
+    public static string GetMessage(Update update)
+    {
+        return update.Type switch
+        {
+            UpdateType.Message => update.Message.Text,
+            UpdateType.CallbackQuery => update.CallbackQuery.Data,
+            _ => null
+        };
+    }
+}
