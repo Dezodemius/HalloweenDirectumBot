cc21ae48e64a97c5ddf6f79322945374eed9d2c4
Dezodemius <gladkovyegor@gmail.com>
2024-04-09 01:00:43 +0400

    remove unused commands

--- DirectumCareerNightBot/BotChatCommands.cs
+++ DirectumCareerNightBot/BotChatCommands.cs
@@ -5,16 +5,11 @@ public class BotChatCommands
     public const string Start = "/start";
     public const string MainMenu = "/menu";
     public const string WantToPractice = "WantToPractice";
-    public const string NotStudentButWantInIT = "NotStudentButWantInIT";
     public const string WorkInITButWantToChangeCompany = "WorkInITButWantToChangeCompany";
     public const string StudentWithExperience = "StudentWithExperience";
     public const string WorkerInITDept = "WorkerInITDept";
     public const string Directum15Questions = "Directum15Questions";
     public const string RafflePrizes = "RafflePrizes";
-    public const string Career = "Career";
-    public const string DirectumVK = "DirectumVK";
     public const string Student = "Student";
     public const string NotStudent = "NotStudent";
-    public const string YesWork = "YesWork";
-    public const string NoWork = "NoWork";
 }
